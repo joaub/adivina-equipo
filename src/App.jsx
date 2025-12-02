029c7f6e1660cc1fd9fcfe9bf5b4aedea55db6d5
Joaquin Ugarte <ugartejoa12@gmail.com>
2025-12-02 15:03:33 -0300

    boton siguiente y arreglando boton incorrecto

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
   const [mensaje, setMensaje] = useState("");
 
   const verificar = () => {
+    if (respuesta.trim() === "") {
+      setMensaje("respuesta vacia");
+      return;
+    }
     if (respuesta.trim().toLowerCase() === equipoActual.nombre.toLowerCase()) {
       setMensaje("correcto");
     } else {
@@ -16,39 +20,62 @@ function App() {
     }
   };
 
+  const siguiente = () => {
+    setEquipoActual(EQUIPOS[Math.floor(Math.random() * EQUIPOS.length)]);
+    setRespuesta("");
+    setMensaje(null);
+  };
+
   return (
     <>
-      <div className={`absolute inset-0  text-center  ${darkMode ? "bg-gray-700 text-white" : "bg-gray-200 text-black"
+      <div className={`min-h-screen flex flex-col items-center p-5  ${darkMode ? "bg-gray-700 text-white" : "bg-gray-200 text-black"
         } `}>
-        <h1 className="text-3xl font-bold">Adivina el equipo por el escudo</h1>
+        <h1 className="text-3xl font-bold p-4">Adivina el equipo por el escudo</h1>
         <button onClick={() => setDarkMode(!darkMode)} className={`px-2 absolute top-5 right-60 border  ${darkMode ? "bg-dark" : "bg-white"}`}  >
           modo {darkMode ? 'oscuro' : 'claro'}
         </button>
         <div>
           <img src={equipoActual.img} alt={EQUIPOS.nombre}
-            className='w-40 h-40 p-2' />
+            className='w-50 h-50 items-center rounded-xl blur-sm m-4' />
 
-          <input
-            value={respuesta}
-            onChange={(e) => setRespuesta(e.target.value)}
-            placeholder="Escribe el nombre del equipo..."
-            className="p-3 w-72 rounded-xl border border-gray-400 text-center"
-          />
-          <button
-            onClick={verificar}
-            className="px-10 p-2 border ml-4 rounded-xl bg-blue-500"
-          >
-            Verificar
-          </button>
-          {mensaje === "correcto" && (
-            <p className="text-green-400 text-xl font-semibold">¡Correcto! 🎉</p>
+          <div className='p-5 mt-7'>
+            <input
+              value={respuesta}
+              onChange={(e) => setRespuesta(e.target.value)}
+              placeholder="Escribe el nombre del equipo..."
+              className={` w-72 rounded-xl border border-gray-400 text-center  ${darkMode ? "bg-gray-600 text-white" : "bg-white text-black"}`}
+            />
+            <button
+              onClick={verificar}
+              className="px-10  border ml-4 rounded-xl bg-blue-500"
+            >
+              Verificar
+            </button>
+            {mensaje === "correcto" && (
+              <p className="text-green-400 text-xl font-semibold">¡Correcto! 🎉</p>
+            )}
+
+            {mensaje === "incorrecto" && (
+              <p className="text-red-400 text-xl font-semibold">
+                Incorrecto 😢 intenta otra vez
+              </p>
+            )}
+            {mensaje === "respuesta vacia" && (
+              <p className="text-yellow-400 text-xl font-semibold">
+                Por favor ingresa una respuesta ⚠️
+              </p>
+            )}
+
+          </div>
+          {mensaje !== "respuesta vacia" && mensaje !== "" && mensaje !== "incorrecto" &&(
+            <button
+              onClick={siguiente}
+              className="px-6 py-2 border rounded-lg "
+            >
+              Siguiente
+            </button>
           )}
 
-          {mensaje === "incorrecto" && (
-            <p className="text-red-400 text-xl font-semibold">
-              Incorrecto 😢 Era: {equipoActual.nombre}
-            </p>
-          )}
         </div>
       </div>
 
--- src/componentes/equipos.js
+++ src/componentes/equipos.js
@@ -3,5 +3,11 @@ export const EQUIPOS = [
         nombre: "Boca Juniors",
         id:1,
         img:"/public/escudos/boca.png",
+    },
+    {
+        nombre: "Real Madrid",
+        id:2,
+        img:"/public/escudos/real-madrid.png",
     }
+    
 ]
